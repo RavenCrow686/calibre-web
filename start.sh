310b58afb17b92de0dddfaebfd9dd18f694fd6b4
RavenCrow686 <homechemmxli@gmail.com>
2025-04-08 22:19:55 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,21 +5,8 @@ echo "Sincronización completada."
 
 # Inicia Calibre-Web
 #/usr/bin/calibre-web
-echo "Listando /usr/bin:"
-ls -l /usr/bin/calibre*
-echo "Listando /opt/calibre-web:"
-ls -l /opt/calibre-web/calibre*
-echo "Listando /app:"
-ls -l /app/calibre*
-echo "Listando /usr/local/bin:"
-ls -l /usr/local/bin/calibre*
-echo "Listando la raíz /:"
-ls -l /calibre*
 
-# Intenta iniciar Calibre-Web desde una posible ubicación
-#/opt/calibre-web/calibre-web &
-# Si la anterior falla, intenta esta:
-# /app/calibre-web/calibre-web &
-echo "Intentando iniciar Calibre-Web con python3 cps.py..."
-python3 /app/config/cps.py &
-echo "Calibre-Web iniciado (¿quizás?)."
+echo "Intentando iniciar Calibre-Web con python3 cps.py y redirigiendo la salida..."
+python3 /app/config/cps.py &> calibre-web.log &
+echo "Calibre-Web iniciado (¿quizás?). Logs deberían estar en calibre-web.log."
+
