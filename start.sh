ad21ef0156965147412fd89cc3f4df9f22232809
RavenCrow686 <homechemmxli@gmail.com>
2025-04-08 21:32:38 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,13 @@ rclone sync gdrive:/books /books --config /app/config/rclone.conf
 echo "Sincronización completada."
 
 # Inicia Calibre-Web
-/usr/bin/calibre-web
+#/usr/bin/calibre-web
+echo "Listando /usr/bin:"
+ls -l /usr/bin/calibre*
+ls -l /opt/calibre-web/
+ls -l /app/calibre-web/
+
+# Intenta iniciar Calibre-Web desde una posible ubicación
+/opt/calibre-web/calibre-web &
+# Si la anterior falla, intenta esta:
+# /app/calibre-web/calibre-web &
